77fceb14a2f910382b3185ee57a96854fb9dcf38
kck <kckateera@gmail.com>
2019-07-23 17:32:38 +0200

    modified:   prac1.py - code refactoring and commenting

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -16,20 +16,26 @@ import RPi.GPIO as GPIO, time, itertools
 
 # Logic that you write
 def main():
-    #print("write your logic here")
-    GPIO.setwarnings(False)
-    out_pins = [3,5,7]
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(out_pins, GPIO.OUT)
+    #initialize pins as desired
+    init_pins()
+    #create list of binary numbers
     binaryVals = list(itertools.product([0,1], repeat = 3))
-    print(binaryVals)
+    print(binaryVals) #checking
     
     for i in binaryVals:
-        GPIO.output(out_pins, i)
+        GPIO.output(out_pins, i) #assign count value
         time.sleep(2)
-        GPIO.output(out_pins, 0)
+        GPIO.output(out_pins, 0) #switch off all LEDs
         time.sleep(2)    
 
+def init_pins():
+    GPIO.setwarnings(False) #stop pi from displaying config warning errors
+    out_pins = [3,5,7] #list of output pins
+    in_pins = [] #list of intput pins
+    GPIO.setmode(GPIO.BOARD) #set board numbering system
+    GPIO.setup(out_pins, GPIO.OUT)
+    
+    
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
