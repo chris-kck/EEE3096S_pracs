afbf9868cb668c0148020dea18eafa4ab66a27cf
kck <kckateera@gmail.com>
2019-07-23 17:02:42 +0200

    modified:   prac1.py - toggling 3 LEDs

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -3,19 +3,28 @@
 Python Practical Template
 Keegan Crankshaw
 Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: Chris Kateera
+Student Number: KTRKUD001
+Prac: Prac 01
+Date: 23/07/2019
 """
 
 # import Relevant Librares
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO, time
 
 # Logic that you write
 def main():
-    print("write your logic here")
-
+    #print("write your logic here")
+    GPIO.setwarnings(False)
+    out_pins = [3,5,7]
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(out_pins, GPIO.OUT)
+    for i in range (5):
+        GPIO.output(out_pins, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(out_pins, GPIO.LOW)
+        time.sleep(1)
+    
 
 # Only run the functions if 
 if __name__ == "__main__":
