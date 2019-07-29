035ef6d33f4d374067edf0eeda863a2b49bd5cc7
kck <kckateera@gmail.com>
2019-07-30 00:26:03 +0200

    Working 3-bit counter using interrupts to count up / down.

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -11,7 +11,11 @@ Date: 23/07/2019
 
 # import Relevant Librares
 import RPi.GPIO as GPIO, time, itertools
-
+binaryVals = list(itertools.product([0,1], repeat = 3))
+index=0
+out_pins = [3,5,7] #list of output pins
+in_pins = [11,13] #list of intput pins
+print(binaryVals)
 
 
 # Logic that you write
@@ -32,41 +36,33 @@ def main():
         time.sleep(2)   '''
 
 def count_up(pin):
-    index=0 #***********************************************
     time.sleep(.01)
     if GPIO.input(pin):
-        while 1:
-            GPIO.output(out_pins, binaryVals[index])
-            index+=1
-            time.sleep(2)
-            GPIO.output(out_pins, 0) #switch off all LEDs
-            time.sleep(2)
-            if index==8:
-                index=0
+        global index, out_pins
+        index+=1
+        GPIO.output(out_pins, binaryVals[index])
+        time.sleep(1)
+        #GPIO.output(out_pins, 0) #switch off all LEDs
+        #time.sleep(1)
+        if index==7:
+            index=0
     
     
 def count_down(pin):
-    index=0
     time.sleep(.01)
     if GPIO.input(pin):
-        while 1:
-            GPIO.output(out_pins, binaryVals[index])
-            index-=1
-            time.sleep(2)
-            GPIO.output(out_pins, 0) #switch off all LEDs
-            time.sleep(2)
-            if index ==0:
-                index=8
+        global index, out_pins
+        index-=1
+        GPIO.output(out_pins, binaryVals[index])
+        time.sleep(2)
+        #GPIO.output(out_pins, 0) #switch off all LEDs
+        #time.sleep(2)
+        if index ==0:
+            index=7
 
 def init_pins():
     GPIO.setwarnings(False) #stop pi from displaying config warning errors
-    # Make sure the GPIO is stopped correctly
-    GPIO.setmode(GPIO.BOARD) #set board numbering system
-      
-    global out_pins, init_pins
-    out_pins = [3,5,7] #list of output pins
-    in_pins = [11,13] #list of intput pins
-    
+    GPIO.setmode(GPIO.BOARD) #set board numbering system    
     GPIO.setup(out_pins, GPIO.OUT, initial=0)
     # Wire the button to +3.3, then enable an internal pulldown.
     GPIO.setup(in_pins, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -74,14 +70,9 @@ def init_pins():
     
 # Only run the functions if 
 if __name__ == "__main__":
-    init_pins()
-    
-    global binaryVals, index
-    binaryVals = list(itertools.product([0,1], repeat = 3))
-    print(binaryVals)
-    
-    GPIO.add_event_detect(11, GPIO.BOTH, callback=count_up, bouncetime=300) #count up
-    GPIO.add_event_detect(13, GPIO.BOTH, callback=count_down, bouncetime=300) #count down
+    init_pins()    
+    GPIO.add_event_detect(11, GPIO.BOTH, callback=count_up, bouncetime=200) #count up
+    GPIO.add_event_detect(13, GPIO.BOTH, callback=count_down, bouncetime=200) #count down
     
     try:
         time.sleep(1000)
@@ -90,6 +81,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
+        # Make sure the GPIO is stopped correctly
         GPIO.cleanup()
     except Exception as e:
         GPIO.cleanup()
