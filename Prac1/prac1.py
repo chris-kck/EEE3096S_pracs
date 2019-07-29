451dd9faaf9cf31b96fe33f0e58b5762c17081c2
kck <kckateera@gmail.com>
2019-07-29 21:54:18 +0200

    Added both buttons and callback functions

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -17,16 +17,13 @@ import RPi.GPIO as GPIO, time, itertools
 # Logic that you write
 def main():
     #initialize pins as desired
-    init_pins()
     #create list of binary numbers
+    '''
     global binaryVals, index
     binaryVals = list(itertools.product([0,1], repeat = 3))
     print(binaryVals) #checking
+    '''
     
-    # Wire the button to +3.3, then enable an internal pulldown.
-    GPIO.setup(in_pins, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(11, GPIO.BOTH, callback=count_up, bouncetime=300) #count up
-    GPIO.add_event_detect(13, GPIO.BOTH, callback=count_down, bouncetime=300) #count down
     '''
     for i in binaryVals:
         GPIO.output(out_pins, i) #assign count value
@@ -35,42 +32,61 @@ def main():
         time.sleep(2)   '''
 
 def count_up(pin):
+    index=0 #***********************************************
     time.sleep(.01)
     if GPIO.input(pin):
-        GPIO.output(out_pins, binaryVals[index])
-        index+=1
-        time.sleep(2)
-        GPIO.output(out_pins, 0) #switch off all LEDs
-        time.sleep(2)
-        if index==7:
-            index=0
+        while 1:
+            GPIO.output(out_pins, binaryVals[index])
+            index+=1
+            time.sleep(2)
+            GPIO.output(out_pins, 0) #switch off all LEDs
+            time.sleep(2)
+            if index==8:
+                index=0
     
     
 def count_down(pin):
+    index=0
     time.sleep(.01)
     if GPIO.input(pin):
-        GPIO.output(out_pins, binaryVals[index])
-        index-=1
-        time.sleep(2)
-        GPIO.output(out_pins, 0) #switch off all LEDs
-        time.sleep(2)
+        while 1:
+            GPIO.output(out_pins, binaryVals[index])
+            index-=1
+            time.sleep(2)
+            GPIO.output(out_pins, 0) #switch off all LEDs
+            time.sleep(2)
+            if index ==0:
+                index=8
 
 def init_pins():
     GPIO.setwarnings(False) #stop pi from displaying config warning errors
+    # Make sure the GPIO is stopped correctly
+    GPIO.setmode(GPIO.BOARD) #set board numbering system
+      
     global out_pins, init_pins
     out_pins = [3,5,7] #list of output pins
     in_pins = [11,13] #list of intput pins
-    GPIO.setmode(GPIO.BOARD) #set board numbering system
-    GPIO.setup(out_pins, GPIO.OUT)
-    GPIO.setup(in_pins, GPIO.IN)
+    
+    GPIO.setup(out_pins, GPIO.OUT, initial=0)
+    # Wire the button to +3.3, then enable an internal pulldown.
+    GPIO.setup(in_pins, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     
     
 # Only run the functions if 
 if __name__ == "__main__":
-    # Make sure the GPIO is stopped correctly
+    init_pins()
+    
+    global binaryVals, index
+    binaryVals = list(itertools.product([0,1], repeat = 3))
+    print(binaryVals)
+    
+    GPIO.add_event_detect(11, GPIO.BOTH, callback=count_up, bouncetime=300) #count up
+    GPIO.add_event_detect(13, GPIO.BOTH, callback=count_down, bouncetime=300) #count down
+    
     try:
-        while True:
-            main()
+        time.sleep(1000)
+        #while True:
+         #   main()
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
